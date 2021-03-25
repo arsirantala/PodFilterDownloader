1f5a26879b2901a012327547abe9a2b4b526541c
Arsi Rantala <arsi.rantala@kolumbus.fi>
2021-03-25 10:53:25 +0200

    Initial version of the application

---
+++ frmMain.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace PodFilterDownloader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMoreInfoOnSelectedFilter = new System.Windows.Forms.Button();
+            this.btnDownloadSelectedFilter = new System.Windows.Forms.Button();
+            this.lbAvailableFilters = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBrowsePoDInstallLoc = new System.Windows.Forms.Button();
+            this.txtPodInstallationLoc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAuthor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnMoreInfoOnSelectedFilter);
+            this.groupBox1.Controls.Add(this.btnDownloadSelectedFilter);
+            this.groupBox1.Controls.Add(this.lbAvailableFilters);
+            this.groupBox1.Controls.Add(this.progressBar);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(29, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(934, 481);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Click on button to download PoD filter";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(465, 74);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(0, 17);
+            this.lblAuthor.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Author";
+            // 
+            // btnMoreInfoOnSelectedFilter
+            // 
+            this.btnMoreInfoOnSelectedFilter.Location = new System.Drawing.Point(708, 133);
+            this.btnMoreInfoOnSelectedFilter.Name = "btnMoreInfoOnSelectedFilter";
+            this.btnMoreInfoOnSelectedFilter.Size = new System.Drawing.Size(198, 56);
+            this.btnMoreInfoOnSelectedFilter.TabIndex = 2;
+            this.btnMoreInfoOnSelectedFilter.Text = "More info on selected filter";
+            this.btnMoreInfoOnSelectedFilter.UseVisualStyleBackColor = true;
+            this.btnMoreInfoOnSelectedFilter.Click += new System.EventHandler(this.btnMoreInfoOnSelectedFilter_Click);
+            // 
+            // btnDownloadSelectedFilter
+            // 
+            this.btnDownloadSelectedFilter.Location = new System.Drawing.Point(708, 35);
+            this.btnDownloadSelectedFilter.Name = "btnDownloadSelectedFilter";
+            this.btnDownloadSelectedFilter.Size = new System.Drawing.Size(198, 56);
+            this.btnDownloadSelectedFilter.TabIndex = 1;
+            this.btnDownloadSelectedFilter.Text = "Download selected filter";
+            this.btnDownloadSelectedFilter.UseVisualStyleBackColor = true;
+            this.btnDownloadSelectedFilter.Click += new System.EventHandler(this.btnDownloadSelectedFilter_Click);
+            // 
+            // lbAvailableFilters
+            // 
+            this.lbAvailableFilters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAvailableFilters.FormattingEnabled = true;
+            this.lbAvailableFilters.ItemHeight = 25;
+            this.lbAvailableFilters.Location = new System.Drawing.Point(33, 35);
+            this.lbAvailableFilters.Name = "lbAvailableFilters";
+            this.lbAvailableFilters.Size = new System.Drawing.Size(400, 154);
+            this.lbAvailableFilters.TabIndex = 0;
+            this.lbAvailableFilters.SelectedIndexChanged += new System.EventHandler(this.lbAvailableFilters_SelectedIndexChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(33, 230);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(873, 43);
+            this.progressBar.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnBrowsePoDInstallLoc);
+            this.groupBox2.Controls.Add(this.txtPodInstallationLoc);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(33, 315);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(873, 130);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PoD install location";
+            // 
+            // btnBrowsePoDInstallLoc
+            // 
+            this.btnBrowsePoDInstallLoc.Location = new System.Drawing.Point(398, 70);
+            this.btnBrowsePoDInstallLoc.Name = "btnBrowsePoDInstallLoc";
+            this.btnBrowsePoDInstallLoc.Size = new System.Drawing.Size(75, 31);
+            this.btnBrowsePoDInstallLoc.TabIndex = 2;
+            this.btnBrowsePoDInstallLoc.Text = "...";
+            this.btnBrowsePoDInstallLoc.UseVisualStyleBackColor = true;
+            this.btnBrowsePoDInstallLoc.Click += new System.EventHandler(this.btnBrowsePoDInstallLoc_Click);
+            // 
+            // txtPodInstallationLoc
+            // 
+            this.txtPodInstallationLoc.Enabled = false;
+            this.txtPodInstallationLoc.Location = new System.Drawing.Point(13, 71);
+            this.txtPodInstallationLoc.Name = "txtPodInstallationLoc";
+            this.txtPodInstallationLoc.Size = new System.Drawing.Size(354, 22);
+            this.txtPodInstallationLoc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PoD installation directory";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFileExit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 24);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItemFileExit
+            // 
+            this.toolStripMenuItemFileExit.Name = "toolStripMenuItemFileExit";
+            this.toolStripMenuItemFileExit.Size = new System.Drawing.Size(108, 26);
+            this.toolStripMenuItemFileExit.Text = "Exit";
+            this.toolStripMenuItemFileExit.Click += new System.EventHandler(this.toolStripMenuItemFileExit_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemHelpAbout});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(53, 24);
+            this.toolStripMenuItem2.Text = "Help";
+            // 
+            // toolStripMenuItemHelpAbout
+            // 
+            this.toolStripMenuItemHelpAbout.Name = "toolStripMenuItemHelpAbout";
+            this.toolStripMenuItemHelpAbout.Size = new System.Drawing.Size(134, 26);
+            this.toolStripMenuItemHelpAbout.Text = "About...";
+            this.toolStripMenuItemHelpAbout.Click += new System.EventHandler(this.toolStripMenuItemHelpAbout_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 250;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 541);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PoD filter downloader";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnBrowsePoDInstallLoc;
+        private System.Windows.Forms.TextBox txtPodInstallationLoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFileExit;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelpAbout;
+        private System.Windows.Forms.Button btnDownloadSelectedFilter;
+        private System.Windows.Forms.ListBox lbAvailableFilters;
+        private System.Windows.Forms.Button btnMoreInfoOnSelectedFilter;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using IniParser;
+using IniParser.Model;
+
+namespace PodFilterDownloader
+{
+    public partial class frmMain : Form
+    {
+        private FileIniDataParser _parser = new FileIniDataParser();
+        private IniData _data;
+        private string _selectedFilter;
+
+        private string[] _availFilters = new string[0];
+        private string[] _availFiltersUrls = new string[0];
+        private string[] _availFiltersHomeUrls = new string[0];
+        private string[] _availFiltersAuthors = new string[0];
+        private int _lastSelectedFilterIndx;
+        private List<string> _downloadedFilterEtags = new List<string>();
+        private List<string> _availableFilterEtags = new List<string>();
+
+        public string[] AvailFilters { get => _availFilters; set => _availFilters = value; }
+        public string[] AvailFiltersUrls { get => _availFiltersUrls; set => _availFiltersUrls = value; }
+        public string[] AvailFiltersHomeUrls { get => _availFiltersHomeUrls; set => _availFiltersHomeUrls = value; }
+        public string[] AvailFiltersAuthors { get => _availFiltersAuthors; set => _availFiltersAuthors = value; }
+        public List<string> DownloadedFilterEtags { get => _downloadedFilterEtags; set => _downloadedFilterEtags = value; }
+        public List<string> AvailableFilterEtags { get => _availableFilterEtags; set => _availableFilterEtags = value; }
+        public int LastSelectedFilterIndx { get => _lastSelectedFilterIndx; set => _lastSelectedFilterIndx = value; }
+
+        private void DownloadFilterFile(string url)
+        {
+            /* if (Available_filter_etags[lbAvailableFilters.SelectedIndex] == downloaded_filter_etags[lbAvailableFilters.SelectedIndex])
+            {
+                if (MessageBox.Show("The downloaded file is the same. Do you want to re download it?", "Info", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) != DialogResult.Yes)
+                {
+                    btnDownloadSelectedFilter.Enabled = true;
+                    btnBrowsePoDInstallLoc.Enabled = true;
+                    return;
+                }
+            } */
+
+            if (txtPodInstallationLoc.Text.Trim().Length == 0)
+            {
+                btnDownloadSelectedFilter.Enabled = true;
+                btnBrowsePoDInstallLoc.Enabled = true;
+                MessageBox.Show(@"You need to define the install location", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (File.Exists($"{Path.GetTempPath()}\\{lblAuthor.Text}_{_selectedFilter}_item.filter"))
+            {
+                if (DownloadedFilterEtags[lbAvailableFilters.SelectedIndex] == AvailableFilterEtags[lbAvailableFilters.SelectedIndex])
+                {
+                    // File in temp is the same as one in the server, so just copy the file to PoD filter directory
+                    File.Copy($"{Path.GetTempPath()}\\{lblAuthor.Text}_{_selectedFilter}_item.filter",
+                        $"{txtPodInstallationLoc.Text}\\filter\\item.filter", true);
+                    MessageBox.Show(@"Already downloaded filter file was copied to Pod filter directory, as it was the same as previously downloaded", @"Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnDownloadSelectedFilter.Enabled = true;
+                    btnBrowsePoDInstallLoc.Enabled = true;
+                    return;
+                }
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileCompleted += DownloadFileCompleted;
+                wc.DownloadFileAsync(new Uri(url),
+                    $"{Path.GetTempPath()}\\{lblAuthor.Text}_{_selectedFilter}_item.filter");
+                // wc.DownloadFileAsync(new Uri(url), txtPodInstallationLoc.Text + "\\filter\\" + _selectedFilter + ".filter");
+            }
+        }
+
+        private void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            // Update the ETag value for the downloaded file, so it can be later compared to the Etag for the same file in the internet
+            WebRequest webRequest = WebRequest.Create(AvailFiltersUrls[lbAvailableFilters.SelectedIndex]);
+            webRequest.Method = "HEAD";
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                DownloadedFilterEtags[lbAvailableFilters.SelectedIndex] = webResponse.Headers["ETag"];
+            }
+
+            File.Copy(Path.GetTempPath() + "\\" + lblAuthor.Text + "_" + _selectedFilter + "_item.filter",
+                txtPodInstallationLoc.Text + "\\filter\\item.filter", true);
+
+            MessageBox.Show(@"Loaded and copied filter file to Pod filter directory", @"Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            btnDownloadSelectedFilter.Enabled = true;
+            btnBrowsePoDInstallLoc.Enabled = true;
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowsePoDInstallLoc_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+
+            DialogResult result = browserDialog.ShowDialog();
+
+            if (result == DialogResult.OK) {
+                txtPodInstallationLoc.Text = browserDialog.SelectedPath;
+            }
+        }
+
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void toolStripMenuItemFileExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            _selectedFilter = lbAvailableFilters.GetItemText(lbAvailableFilters.SelectedItem);
+            txtPodInstallationLoc.Text = Properties.Settings.Default.PoDInstallLocation;
+            AvailFilters = Properties.Settings.Default.AvailableFilters_names.Split(',');
+            AvailFiltersUrls = Properties.Settings.Default.AvailableFilterUrls.Split(',');
+            AvailFiltersHomeUrls = Properties.Settings.Default.AvailableFilterHomeUrls.Split(',');
+            AvailFiltersAuthors = Properties.Settings.Default.AvailableFilterAuthors.Split(',');
+            string[] temp = Properties.Settings.Default.DownloadedFilterEtags.Split(',');
+
+            foreach (string tt in temp)
+            {
+                DownloadedFilterEtags.Add(tt);
+            }
+
+            foreach (string str in AvailFilters)
+            {
+                lbAvailableFilters.Items.Add(str);
+            }
+
+            lbAvailableFilters.SelectedIndex = Properties.Settings.Default.SelectedAvailableFilterIndex;
+            LastSelectedFilterIndx = lbAvailableFilters.SelectedIndex;
+            lblAuthor.Text = AvailFiltersAuthors[lbAvailableFilters.SelectedIndex];
+
+            _data = _parser.ReadFile("Configuration.ini");
+
+            //string useFullScreenStr = data["UI"]["fullscreen"];
+
+            //data["UI"]["fullscreen"] = "true";
+            //parser.WriteFile("Configuration.ini", data);
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default["PodInstallLocation"] = txtPodInstallationLoc.Text;
+            Properties.Settings.Default["SelectedAvailableFilterIndex"] = lbAvailableFilters.SelectedIndex;
+            Properties.Settings.Default["DownloadedFilterEtags"] = string.Join(",", _downloadedFilterEtags.ToArray());
+            Properties.Settings.Default.Save();
+        }
+
+        private void toolStripMenuItemHelpAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                $@"PoD filter downloader by Ixoth{Environment.NewLine}Version: {Application.ProductVersion}{Environment.NewLine}{Environment.NewLine}Copyright (C) 2021{Environment.NewLine}All rights reserved", 
+                @"About...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDownloadSelectedFilter_Click(object sender, EventArgs e)
+        {
+            btnDownloadSelectedFilter.Enabled = false;
+            DownloadFilterFile(AvailFiltersUrls[lbAvailableFilters.SelectedIndex]);
+        }
+
+        private void btnMoreInfoOnSelectedFilter_Click(object sender, EventArgs e)
+        {
+            if (lbAvailableFilters.SelectedIndex == -1)
+            {
+                MessageBox.Show(@"Please select first some filter in the listbox!", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+      
+            System.Diagnostics.Process.Start(AvailFiltersHomeUrls[lbAvailableFilters.SelectedIndex]);
+        }
+
+        private void lbAvailableFilters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _selectedFilter = lbAvailableFilters.GetItemText(lbAvailableFilters.SelectedItem);
+            lblAuthor.Text = AvailFiltersAuthors[lbAvailableFilters.SelectedIndex];
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Check the ETag values for the filters, so that filters which have been changed can be indicated that there might be new version available
+
+            timer.Enabled = false;
+
+            Boolean first = true;
+            foreach(string filter in AvailFiltersUrls)
+            {
+                WebRequest webRequest = WebRequest.Create(filter);
+                webRequest.Method = "HEAD";
+
+                using (WebResponse webResponse = webRequest.GetResponse())
+                {
+                    if (first)
+                    {
+                        Properties.Settings.Default["AvailableFilterEtags"] = webResponse.Headers["ETag"];
+                        first = false;
+                    }
+                    else
+                    {
+                        Properties.Settings.Default["AvailableFilterEtags"] = "," + webResponse.Headers["ETag"];
+                    }
+                    AvailableFilterEtags.Add(webResponse.Headers["ETag"]);
+                }
+            }
+            Properties.Settings.Default.Save();
+
+            // Check the filter files, if they're different in the server vs what has been downloaded in past (to temp directory)
+
+            /* if (downloaded_filter_etags[Last_selected_filter_indx] != Available_filter_etags[Last_selected_filter_indx])
+            {
+                lbAvailableFilters.Items[Last_selected_filter_indx + 1] += " (different in server)";
+            } */
+
+            //int cntr = 0;
+            //int maxLength = lbAvailableFilters.Items.Count;
+
+            //while (cntr < maxLength)
+            //{
+            //    if (DownloadedFilterEtags[cntr] != AvailableFilterEtags[cntr])
+            //    {
+            //        lbAvailableFilters.Items[cntr + 1] += " (file in server is different than one in temp)";
+            //    }
+
+            //    cntr++;
+            //}
+        }
+    }
+}
