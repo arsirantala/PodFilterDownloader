3ad9669a0b59beea4f2f43a5fbf136bd9355fdb4
Arsi Rantala <arsi.rantala@kolumbus.fi>
2021-03-29 16:20:28 +0300

    Rename project

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PodFilterDownloader.Properties {
+namespace IxothPodFilterDownloader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
